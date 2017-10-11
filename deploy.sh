73ebf200e19029b46eb5bf84715da4f9ffb963e2
Matthias Noback <matthiasnoback@gmail.com>
2017-10-11 11:02:12 +0200

    Make the deploy script a bit safer

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
-eval $(docker-machine env $DOCKER_MACHINE_NAME)
+echo "Deploy to ${DOCKER_MACHINE_NAME}..."
+
+eval $(docker-machine env "${DOCKER_MACHINE_NAME}")
 docker network create --driver bridge nginx-proxy || true
 docker-compose pull
 docker-compose up -d --force-recreate --no-build
